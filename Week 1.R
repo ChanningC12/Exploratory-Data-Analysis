150b73c80eb63db0808ef843581c72598fae189a
ChanningC12 <chicheng2015@u.northwestern.edu>
2016-03-11 18:22:44 -0600

    Further study

--- Week 1.R
+++ Week 1.R
@@ -55,7 +55,76 @@ xyplot(Life.Exp~Income | region, data=state, layout=c(4,1))
 library(ggplot2)
 qplot(displ,hwy,data=mpg)
 
+# Base Graphics
+hist(airquality$Ozone)
+with(airquality,plot(Wind,Ozone))
+airquality = transform(airquality,Month=factor(Month)) # Month - Categorical variable
+boxplot(Ozone~Month,airquality,xlab="Month",ylab="Ozone(ppb)")
 
+# Key Parameters:
+# pch: the plotting symbol, default is open circle
+# lty: the line type, default is solid line. Can be dashed, dotted, etc.
+# lwd: the line width, specified as an integer multiple
+# col: the plotting color, specified as a number, string, or hex code; the colors() function gives you a vector of colors by name
+# xlab, ylab
 
+# Basic function
+# plot: scatter plot as default
+# lines: add lines to a plot
+# points: add points to a plot
+# text: add text labels to a plot using specified x,y coordinates
+# title
+# mtext
+# axis
 
+with(airquality,plot(Wind,Ozone))
+title(main="Ozone and Wind in NYC")
+# Base plot with annotation
+with(airquality,plot(Wind,Ozone,main="Ozone and Wind in NYC"))
+with(subset(airquality,Month==5),points(Wind,Ozone,col="blue"))
+with(subset(airquality,Month!=5),points(Wind,Ozone,col="red"))
+legend("topright",pch=1,col=c("blue","red"),legend=c("May","Other Months"))
+model = lm(Ozone~Wind,airquality)
+abline()
 
+# Basic Plotting Demonstration
+x=rnorm(100)
+y=rnorm(100)
+plot(x,y)
+par(mar=c(4,4,2,2))
+plot(x,y)
+plot(x,y,pch=20)
+plot(x,y,pch=19)
+plot(x,y,pch=3)
+plot(x,y,pch=4)  # example(points)
+text(-2,-2,"Qifeng") # assign a label
+legend("topleft",legend="Data",pch=20) # legend
+
+fit = lm(y~x)
+abline(fit,lwd=3,col="blue")
+plot(x,y,xlab="Weight",ylab="Height",main="ScatterPlot",pch=20)
+legend("topright",legend="Data",pch=20) # legend
+abline(fit,lwd=3,col="red")
+z=rpois(100,2)
+par(mfrow=c(2,1))
+plot(x,y,pch=20)
+plot(x,z,pch=19)
+par(mar=c(2,2,1,1))
+
+par(mfrow=c(2,2))
+plot(x,y)
+plot(x,z)
+plot(z,x)
+plot(y,x)
+par(mfcol=c(2,2))
+
+par(mfrow=c(1,1))
+x=rnorm(100)
+y=x+rnorm(100)
+g=gl(2,50)
+g=gl(2,50,labels=c("Male","Female"))
+str(g)
+plot(x,y)
+plot(x,y,type="n")   # make the plot but no content
+points(x[g=="Male"],y[g=="Male"],col="green")
+points(x[g=="Female"],y[g=="Female"],col="blue")
