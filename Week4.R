e2375337620995b4e5733e473ce17e8573383c2e
ChanningC12 <chicheng2015@u.northwestern.edu>
2016-06-04 10:59:29 -0500

    week4

---
+++ Week4.R
@@ -0,0 +1,9 @@
+pm0 = read.csv("Desktop/annual_all_2012.csv")
+# class()
+# str()
+# summary()
+# missing value: are missing value I need to worry about?
+# mean(is.na(x0))
+# dates = as.Date(as.character(dates),"%Y%m%d)
+# subset(pm1,State.Code==36,c(Country.Code,Site.ID))
+# tapply: with(pm1,tapply(Sample.Value,State.Code,mean,na.rm=T))
